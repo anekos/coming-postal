b0e58884f750df2caab11dcf0f2597273ed3b964
anekos <anekos@snca.net>
2019-12-15 17:23:36 +0900

    WIP

---
+++ src/coming_postal/service/japanpost.clj
@@ -0,0 +1,50 @@
+(ns coming-postal.service.japanpost
+  (:require [net.cgrand.enlive-html :as eh :refer [select text attr=]]
+            [clj-http.client :as client]))
+
+
+
+(defmacro selectext [html & selector]
+  `(apply
+    str
+    (map
+     text
+     (select ~html ~@selector))))
+
+(defn make-url [code]
+  (str
+    "https://trackings.post.japanpost.jp/services/srv/search/direct?reqCodeNo1="
+    code
+    "&searchKind=S002&locale=ja"))
+
+(defn get-state-html [code]
+  (-> code
+      make-url
+      client/get
+      :body
+      eh/html-snippet))
+
+(defn extract-log-entries [html]
+  (select html [:body [:table.tableType01 (attr= :summary "履歴情報")] :tr]))
+
+(defn parse-log-entry [e]
+  {:at
+   (selectext e [:td.w_120])
+   :state
+   (selectext e [:td.w_150])
+   :detail
+   (selectext e [:td.w_180])})
+
+(defn valid-entry? [e]
+  (not
+   (or (empty? (:at e))
+       (empty? (:state e)))))
+
+
+(defn get-state [code]
+  (->>
+   (-> code
+       get-state-html
+       extract-log-entries)
+   (map parse-log-entry)
+   (filter valid-entry?)))
