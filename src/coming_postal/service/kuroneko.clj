79db314316593f99999db981151e930a764ace97
anekos <anekos@snca.net>
2019-12-17 23:14:55 +0900

    WIP

---
+++ src/coming_postal/service/kuroneko.clj
@@ -0,0 +1,25 @@
+(ns coming-postal.service.kuroneko
+  (:gen-class)
+  (:require [clj-http.client :as client]
+            [clojure.string :as string :refer [trim]]
+            [coming-postal.service.core :refer [get-log]]
+            [net.cgrand.enlive-html :refer [html-snippet select text]]))
+
+
+(def url "http://link.kuronekoyamato.co.jp/link/send/receive/lneko")
+
+
+(defn get-log-html [code]
+  (-> url
+      (client/post
+        {:headers
+         {:User-Agent "Mozilla/5.0 (X11; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0"}}
+        {:form-params
+         {:number00 "1"
+          :number01 code
+          :userid "600121"
+          :n01 code}
+         :decode-body-headers true
+         :as :auto})
+      :body
+      html-snippet))
