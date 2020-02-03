d4f44d9dd39468dd6e0eaafe4760b57237e8864a
anekos <anekos@snca.net>
2020-02-03 23:39:04 +0900

    Use `cachify`

--- project.clj
+++ project.clj
@@ -9,7 +9,8 @@
                  [clojure.java-time "0.3.0"]
                  [net.snca/kunekune "0.1.6"]
                  [me.raynes/fs "1.4.6"]
-                 [org.clojure/core.cache "0.8.2"]]
+                 [org.clojure/core.cache "0.8.2"]
+                 [net.snca/cachify "0.1.0"]]
   :main coming-postal.core
   :profiles {:dev
              {:dependencies [[org.clojure/tools.namespace "0.3.1"]]
--- src/coming_postal/agent.clj
+++ src/coming_postal/agent.clj
@@ -3,36 +3,10 @@
             [clojure.edn :as edn]
             [clj-http.client :as http-client]
             [clojure.java.io :refer [file]]
-            [me.raynes.fs :as fs])
+            [me.raynes.fs :as fs]
+            [cachify.core :refer [cachify]])
   (:refer-clojure :exclude [get]))
 
 
-(def ^:dynamic *cache-file* (file (fs/home) ".local/var/cache/coming-postal/http-agent"))
-
-(def ^:dynamic *cache*
-  (atom (cache/ttl-cache-factory
-          (if (.exists *cache-file*)
-            (-> *cache-file*
-                slurp
-                edn/read-string)
-            {})
-          :ttl (* 1000 60 60))))
-
-(defn store-cache []
-  (fs/mkdirs (.getParent *cache-file*))
-  (spit *cache-file*
-        @*cache*))
-
-(defn with-cache [f]
-  (fn [& args]
-    (cache/lookup
-      (swap!
-       *cache*
-       cache/through-cache
-       args
-       (fn [args]
-         (apply f args)))
-     args)))
-
-(def get (with-cache (comp :body http-client/get)))
-(def post (with-cache (comp :body http-client/post)))
+(def get (cachify :get (comp :body http-client/get)))
+(def post (cachify :post (comp :body http-client/post)))
